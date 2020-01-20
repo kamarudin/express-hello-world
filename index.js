d75aa2db470f37ed8bf0b2a3a668612fb6d4a512
Kamarudin Samsudin <kamarudin@gmail.com>
2020-01-20 17:01:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World! Test!!!!!!')
+  res.send('Hello World! Test!!!!!! test2!')
 })
 
 app.listen(8080, function () {
